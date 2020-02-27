a8df221d8037b51d25c665c996d3dd4f2e9cf304
Alexter-progs <alexplotbox@gmail.com>
2020-02-27 12:26:56 +0300

    add koa with typescript config for it

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+
+const app = new Koa();
+
+
+app.use(async (ctx: any, next: any) => {
+    // Log the request to the console
+    console.log('Url:', ctx.url);
+    // Pass the request to the next middleware function
+    await next();
+});
+
+const router = new Router();
+router.get('/*', async (ctx: any) => { 
+    ctx.body = 'Hello my friend';
+});
+
+app.use(router.routes());
+
+app.listen(3000);
+
+console.log('Running server on port 3000');
