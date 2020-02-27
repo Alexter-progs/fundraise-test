46f024e5162ba4ebdc796313eb74f8e44977fb6a
Alexter-progs <alexplotbox@gmail.com>
2020-02-27 22:56:12 +0300

    dummy currency input component

---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Currency input 
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Button extends Vue {
+   
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+</style>
