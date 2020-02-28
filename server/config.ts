1ec79aaea46bd2d6f59cb87b636f8a798646dde3
Alexter-progs <alexplotbox@gmail.com>
2020-02-28 13:48:49 +0300

    saving donation to mongodb

---
+++ server/config.ts
@@ -0,0 +1,5 @@
+export default {
+    PORT: 3000,
+    MONGODB_URL: 'mongodb://localhost:27017',
+    DB_NAME: 'fundraise'
+}
--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,17 @@ import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as serve from 'koa-static';
 import * as bodyParser from 'koa-body';
+import { MongoClient, Collection } from 'mongodb';
 
 import * as path from 'path';
 
+import config from './config';
+
 const app = new Koa();
+const client = new MongoClient(config.MONGODB_URL, { useUnifiedTopology: true });
 const FRONTEND_APP_BUILD_PATH = path.resolve(__dirname, '../dist');
+
+
 
 app.use(bodyParser());
 
@@ -26,12 +32,25 @@ router.post('/api/donate', async (ctx: any) => {
     const {amount, currency} = ctx.request.body;
 
     if (amount > 0 && allowedCurrencies.some((allowedCurrency) => currency === allowedCurrency)) {
+        const collection: Collection = ctx.db.collection('donations');
+
+        await collection.insertOne({
+            amount,
+            currency
+        });
+
         ctx.body = JSON.stringify({"ok": true});
     }
 });
 
 app.use(router.routes());
 
-app.listen(3000);
+client.connect((err) => {
+    if (!err) {
+        const db = client.db(config.DB_NAME);
+        app.context.db = db;
+    }
+});
 
-console.log('Running server on port 3000');
+app.listen(config.PORT);
+console.log(`Running server on port ${config.PORT}`);
